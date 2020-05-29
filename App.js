f8cb3c58d6d8d0ac7ec2ec9ad2a9704ef1f59a28
Param Shah <paramshah09@gmail.com>
2020-05-29 23:35:13 +0530

    disable zoom in webview

--- App.js
+++ App.js
@@ -2,18 +2,15 @@ import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
-
 export default function App() {
+  const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'initial-scale=1, maximum-scale=1, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);`
   return (
     <View style={styles.container}>
-      <WebView 
+      <WebView
+        originWhitelist={['*']}
         source={{ uri: 'https://warriorwellness.me/' }}
         style={{marginTop: 30}}
-        scalesPageToFit={true}
+        injectedJavaScript={INJECTEDJAVASCRIPT}
       />
     </View>
   );
@@ -23,17 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
